e14860d99b60e6efb3a8b67fea56d2090641582d
Salvatore Laisa <moebiusmania@gmail.com>
2018-03-27 21:38:41 +0200

    moving jsonp utility inside project

--- src/libs.js
+++ src/libs.js
@@ -1,12 +1,12 @@
 
-import fetchJsonp from 'fetch-jsonp';
+import jsonp from './fetchjsonp';
 
 const extend = (obj, params) => Object.assign({}, obj, params);
 
 export default (user, uuid) => {
   const url = `https://gist.github.com/${user}/${uuid}.json`;
 
-  return fetchJsonp(url)
+  return jsonp(url)
     .then(res => res.json())
     .then(data => extend(data, {loading: false}));
 };
